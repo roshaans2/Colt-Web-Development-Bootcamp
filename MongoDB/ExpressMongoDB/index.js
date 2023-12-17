81f81b657e61f22ad191b50b5b0381a112b44ede
roshaans2 <roshaansathya@gmail.com>
2023-12-17 23:10:10 +0530

    Wow, EJS + Expres + MongoDB is awesome

--- MongoDB/ExpressMongoDB/index.js
+++ MongoDB/ExpressMongoDB/index.js
@@ -1,24 +1,88 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine','ejs')
-
-const mongoose = require("mongoose")
 require('dotenv').config()
-const MONGO_URI = process.env.MONGO_URI
-mongoose.connect(MONGO_URI)
-.then(()=>{
-    console.log("DB Connected")
+
+
+const Product = require('./models/product');
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("DB Connected")
+    })
+    .catch(err => {
+        console.log("Error")
+        console.log(err)
+    })
+
+
+const categories = ['fruit','vegetable','dairy']
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.get("/products/new",(req, res) => {
+    res.render('products/new',{categories})
+});
+
+app.get("/products",async (req,res)=>{
+    const {category} = req.query
+    if(category){
+        const products = await Product.find({category:category})
+        res.render("products/index",{products,category})
+    }
+    else{
+        const products = await Product.find({})
+        res.render("products/index",{products,category:'All'})
+    }
 })
-.catch((err)=>{
-    console.log(err);
+
+
+app.get("/products/:id",async(req,res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render("products/show",{product})
+})
+
+app.get("/products/:id/edit", async (req,res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render("products/edit",{product,categories})
+})
+
+app.post("/products",async(req,res)=>{
+    const newProduct = Product(req.body)
+    newProduct.save()
+    console.log(req.body)
+    res.redirect("/products")
+})
+
+app.put("/products/:id", async(req,res)=>{
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+    res.redirect(`/products/${product.id}`)
+})
+
+app.delete("/products/:id",async(req,res)=>{
+    const {id} = req.params;
+    await Product.findByIdAndDelete(id)
+    res.redirect("/products")
+})
+
+app.listen(5000, () => {
+    console.log("Server connected at port 5000")
 })
 
 
 
 
-app.listen(5000,()=>{
-    console.log("Server running at port 5000")
-})
+
+
+
