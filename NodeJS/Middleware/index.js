cd80b610eb109763209978c3f7b6c0ef76900f6b
roshaans2 <roshaansathya@gmail.com>
2023-12-20 22:53:24 +0530

    Some changes

--- NodeJS/Middleware/index.js
+++ NodeJS/Middleware/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const AppError = require("./AppError")
 
 app.use(morgan('common'))
 // app.use((req,res,next)=>{
@@ -24,7 +25,9 @@ const verifyPassword = ((req,res,next)=>{
         next()
     }
     else{
-        res.send("Sorry wrong password")
+        // res.send("Sorry wrong password")
+        // throw new Error("Password Required")
+        throw new AppError("Password required",401)
     }
 })
 
@@ -38,8 +41,25 @@ app.get("/secret",verifyPassword,(req,res)=>{
     res.send("Secret")
 })
 
+app.get("/admin",(req,res)=>{
+    throw new AppError("You are not a admin",403)
+})
+
 app.use((req,res)=>{
     res.status(404).send("Not Found")
+})
+
+//Error handling middleware
+//This middleware is triggered whenever there is an error
+app.use((err,req,res,next)=>{
+    // res.status(500).send("We got an error")
+    // next(err) 
+    //After putting the above line, express default error handler will be triggered
+    //If above line is not given, only error middleware logic will be implemented, default error handler will not be triggered
+    //Only if we add next(), default error handler will be triggered after middleware logic
+
+    const {status=500,message="Something went Wrong"} = err
+    res.status(status).send(message)
 })
 
 app.listen(5000,()=>{
